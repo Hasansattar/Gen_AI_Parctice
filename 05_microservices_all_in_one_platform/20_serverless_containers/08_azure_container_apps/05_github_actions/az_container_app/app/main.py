f50f15ccf1b85d929f02f035ba317222dc5023a4
Hassan Sattar <hasansattar650@gmail.com>
2024-09-22 03:03:14 +0500

    update

--- 05_microservices_all_in_one_platform/20_serverless_containers/08_azure_container_apps/05_github_actions/az_container_app/app/main.py
+++ 05_microservices_all_in_one_platform/20_serverless_containers/08_azure_container_apps/05_github_actions/az_container_app/app/main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1", "World2": "Pakistan zinda bad"}
+    return {"Hello": "World1", "World2": "Pakistan zindabad 22-9-2024"}
